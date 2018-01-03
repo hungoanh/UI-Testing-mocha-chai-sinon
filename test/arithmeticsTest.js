e160dc30ffb6b9dfcff902a2fb5faf51ef1cf304
Hung Le <the.bakonator@outlook.com>
2018-01-02 23:48:45 -0700

    First commit with initial setup

---
+++ test/arithmeticsTest.js
@@ -0,0 +1,8 @@
+//const chai = require('chai');
+const assert = chai.assert;
+
+describe('Arithmetics', function() {
+  it('should be equal to 4 squared', function() {
+    assert.equal(4 * 4, 16);
+  });
+});
---
+++ test/arrayTest.js
@@ -0,0 +1,9 @@
+//const chai = require('chai');
+const assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    const arr = [];
+    assert.equal(arr.length, 0);
+  });
+});
